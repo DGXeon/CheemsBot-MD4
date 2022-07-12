73f19ada7c124b401936f6e5e50f6687f7d8bd66
reddragonnew <109021577+reddragonnew@users.noreply.github.com>
2022-07-12 16:42:20 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -26,20 +26,20 @@ const chalk = require('chalk')
 
 //Api Website\\
 global.APIs = {
-	zenz: 'https://zenzapis.xyz',
+	zenz: 'https://www.remove.bg',
 }
 
 //Api Key\\
 global.APIKeys = {
-	'https://zenzapis.xyz': 'your key',
+	'https://www.remove.bg': 'SF74TKC3kdDt86vDkeFEudZy',
 }
 
 
 //aumto functioner
-global.autoTyping = false //auto tying in gc (true to on, false to off)
-global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
-global.autoReadGc = false //auto reading in gc (true to on, false to off)
-global.autoRecord = false //auto recording (true to on, false to off)
+global.autoTyping = true //auto tying in gc (true to on, false to off)
+global.autoreadpmngc = true //auto reading in gc and pm (true to on, false to off)
+global.autoReadGc = true //auto reading in gc (true to on, false to off)
+global.autoRecord = true //auto recording (true to on, false to off)
 global.available = true //auto available (true to on, false to off)
 
 //semt menu variants
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
+global.vcardowner = ['94740639288'] //ur owner number
+global.ownername = "🦄𝕂𝔸𝕍𝕀𝕊ℍ𝕂𝔸 𝕆𝔽𝔽𝕀ℂ𝕀𝔸𝕃" //ur owner name
+global.ytname = "YT: 𝕂𝔸𝕍𝕀𝕊ℍ𝕂𝔸 𝕆𝔽𝔽𝕀ℂ𝕀𝔸𝕃" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.location = "𝕊ℝ𝕀 𝕃𝔸ℕ𝕂𝔸" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
+global.owner = ['94766439882']
+global.ownertag = '94740639288' //ur tag number
 global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
+global.linkz = "https://youtube.com" //your theme url which will be displayed on whatsapp
+global.websitex = "https://https://chat.whatsapp.com/GMkq5skMhKT105yspDJi80" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.packname = "Cheems Bot Stickers𝕂𝔸𝕍𝕀𝕊ℍ𝕂𝔸 𝕆𝔽𝔽𝕀ℂ𝕀𝔸𝕃🦄🦄🦄 " //ur sticker watermark packname
+global.author = "🦄𝕂𝔸𝕍𝕀𝕊ℍ𝕂𝔸 𝕆𝔽𝔽𝕀ℂ𝕀𝔸𝕃" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
