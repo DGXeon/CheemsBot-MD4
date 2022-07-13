c7e7ed0bc06d79cba93ea9a5d23b8744f30def8c
Iury Xavier <86579744+Iury-Xavier@users.noreply.github.com>
2022-07-12 22:02:09 -0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -31,7 +31,7 @@ global.APIs = {
 
 //Api Key\\
 global.APIKeys = {
-	'https://zenzapis.xyz': 'your key',
+	'https://zenzapis.xyz': '5e4ccb6901',
 }
 
 
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['553288416873'] //ur owner number
+global.ownername = "🔒Iury X." //ur owner name
+global.ytname = "YT: [...]" //ur yt chanel name
+global.socialm = "GitHub: Iury-Xavier" //ur github or insta name
+global.location = "Minas Gerais, Brasil" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
+global.owner = ['553288416873']
+global.ownertag = '553288416873' //ur tag number
+global.botname = 'Agua Bot V2' //ur bot name
 global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
+global.botscript = 'https://github.com/Iury-Xavier/CheemsBot-MD4' //script link
 global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.themeemoji = "💧" //ur theme emoji
+global.packname = "Agua Bot Stickers\n\n\n\n\n\n\n\n\n\n\n💧💧💧 " //ur sticker watermark packname
+global.author = "🔒Iury X." //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['553288416873'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
@@ -102,23 +102,23 @@ global.ntilink = []
 //omther
 global.sessionName = 'session'
 global.antitags = true
-global.prefa = ['','!','.','🐦','🐤','🗿']
+global.prefa = ['','#','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
-    success: 'Done ✓',
+    success: 'Feito ✓',
     admin: 'This Feature Is Only For Admin!',
-    botAdmin: 'Bot Must Be Admin First!',
-    owner: 'This Feature Is Only For Owner!',
-    group: 'Feature Used Only For Groups!',
-    private: 'Features Used Only For Private Chat!',
-    bot: 'This Feature Is Only For Bot',
-    wait: 'In process...',
-    linkm: 'Where is the link?',
-    error: 'Error!!',
-    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
-    ban: 'You have been banned by the owner, if you want to be unbanned, chat owner.',
-    nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
-    banChat: 'The bot was banned in this group, please contact the owner to unban'
+    botAdmin: 'Bot deve ser administrador primeiro!',
+    owner: 'Este recurso é apenas para o dono!',
+    group: 'Recurso usado apenas para grupos!',
+    private: 'Recursos usados apenas para bate-papo privado!',
+    bot: 'Este recurso é apenas para o bot',
+    wait: 'Aguarde...',
+    linkm: 'Cade o link?',
+    error: 'Erro!!',
+    endLimit: 'Seu limite diário expirou, o limite será redefinido a cada 12 horas',
+    ban: 'Você foi banido pelo dono, se quiser ser desbanido entre em contato com o dono.',
+    nsfw: 'O recurso nsfw não foi ativado, entre em contato com o administrador para ativar',
+    banChat: 'O bot foi banido neste grupo, por favor entre em contato com o dono para desbanir'
 }
     global.limitawal = {
     premium: "Infinity",
