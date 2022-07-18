e8cf73e7911cd6fe3fa50156532c0964ade82826
Saradagithu <105852814+Saradagithu@users.noreply.github.com>
2022-07-18 08:37:44 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -56,16 +56,16 @@ global.doc6 = 'application/vnd.android.package-archive'
 //owmner v card
 global.vcardowner = ['916909137213'] //ur owner number
 global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
+global.waname = "IKHWAN" //ur yt chanel name
+global.telegram= "movie group" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
 global.owner = ['916909137213']
 global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
+global.botname = 'IKHWAN BOT' //ur bot name
+global.linkz = "https://wa.link/y17h02" //your theme url which will be displayed on whatsapp
+global.websitex = "https://t.me/+3d20o8e714Y2NjQ1" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
