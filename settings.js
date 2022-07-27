5abe059613da3e3e97bee736cc85f1472455081d
samankavi <106352537+samankavi@users.noreply.github.com>
2022-07-27 19:47:10 +0530

    Update setting.js

--- settings.js
+++ settings.js
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['94771530692'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
