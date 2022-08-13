1aa0fcad643c0b46d50006aa262be0fb9461268f
Abhi-md <110845439+Abhi-md@users.noreply.github.com>
2022-08-13 20:08:51 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,16 +54,16 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['919645640238'] //ur owner number
+global.ownername = "ABHIJITH C" //ur owner name
+global.ytname = "YT: A C SHOT GAMING YT" //ur yt chanel name
+global.socialm = "insta: _abhi_.c" //ur github or insta name
+global.location = "India, Kerala" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
+global.owner = ['919645640238']
+global.ownertag = '919645640238' //ur tag number
+global.botname = 'SILVESTER' //ur bot name
 global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
