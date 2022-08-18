d3a3da1f6611de030c12967d3c11f6c809424d08
AlexPakel <111372564+AlexPakel@users.noreply.github.com>
2022-08-18 16:50:39 +0700

    settings.js
    Fckcjc

--- settings.js
+++ settings.js
@@ -54,14 +54,14 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
+global.vcardowner = ['6282171432358'] //ur owner number
+global.ownername = "Alex" //ur owner name
+global.ytname = "YT: Alex Pakel" //ur yt chanel name
+global.socialm = "GitHub:Alex Pakel" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
+global.owner = ['6282171432358']
 global.ownertag = '916909137213' //ur tag number
 global.botname = 'Cheems Bot MD V4' //ur bot name
 global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
