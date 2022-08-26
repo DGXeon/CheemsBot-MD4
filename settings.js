fe399395afad171d304da6bb3f7ed51ac131bbb3
DulangaThipul <108794574+DulangaThipul@users.noreply.github.com>
2022-08-26 13:51:45 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,8 +54,8 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
+global.vcardowner = ['+94743205172'] //ur owner number
+global.ownername = "Dulanga THIPUL" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
