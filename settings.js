ada68f54b32a27b3d6a12e0cbee9d466f1eac8c2
muhammadtaskeenakhtar <112490541+muhammadtaskeenakhtar@users.noreply.github.com>
2022-09-08 07:23:34 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -55,17 +55,17 @@ global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
 global.vcardowner = ['923216740469] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
+global.ownername = "🦄Taskeen baloch" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.socialm = "Taskeen official" //ur github or insta name
+global.location = "Pakistan khanpur" //ur location
 
 //bot bomdy 
 global.owner = ['923216740469']
 global.ownertag = '923216740469' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
+global.botname = 'Taskewn Bot MD V4' //ur bot name
+global.linkz = "https://youtube.com/channel/UCU0qqOnN46MADk3AzGUe_bQ " //your theme url which will be displayed on whatsapp
+global.websitex = "   https://youtube.com/channel/UCU0qqOnN46MADk3AzGUe_bQ  " //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
