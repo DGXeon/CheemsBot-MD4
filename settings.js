ea0eb51c3fbcc71705b9465826afd44df9eccf6e
Knowladge-hunter <106310599+Knowladge-hunter@users.noreply.github.com>
2022-09-13 11:49:11 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -63,7 +63,7 @@ global.location = "India, Mizoram, Aizawl" //ur location
 //bot bomdy 
 global.owner = ['918292121115']
 global.ownertag = '918292121115' //ur tag number
-global.botname = 'Alen-Ware' //ur bot name
+global.botname = 'Alien-Ware' //ur bot name
 global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
