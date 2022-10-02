7c1777f2003e2f1d8558d0fa88abfef858c19cee
Abanoub121 <53729973+abanoub0120@users.noreply.github.com>
2022-10-02 10:28:57 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
+global.vcardowner = ['201200323527'] //ur owner number
+global.ownername = "🦄Abanoub bebo" //ur owner name
+global.ytname = "HK: Bebo" //ur yt chanel name
+global.socialm = "GitHub: abanoub0120" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
+global.owner = ['201200323527']
+global.ownertag = '201200323527' //ur tag number
+global.botname = 'Cheems Bot hk bebo' //ur bot name
+global.linkz = "https://youtube.com/channel/UCo5FmxZy1KRsDEMhkiLA1lw" //your theme url which will be displayed on whatsapp
+global.websitex = "https://youtube.com/channel/UCo5FmxZy1KRsDEMhkiLA1lw" //ur website to be displayed
+global.botscript = 'https://github.com/abanoub0120/abanoub0120' //script link
+global.reactmoji = "❤️" //ur menu react emoji
+global.themeemoji = "❤️" //ur theme emoji
 global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.author = "❤️Hk bebo" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['201200323527'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
