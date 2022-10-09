0f16e121460724cdb6349228e87a944ccbc9cc3b
SoArLozzy <86555270+SoArLozzy@users.noreply.github.com>
2022-10-09 18:49:51 +0530

    Changed To Corpse Bot
    No des

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['6289372021'] //ur owner number
+global.ownername = "🦄Dream Guy Lucifer_" //ur owner name
+global.ytname = "YT: SoArLozzy_xD" //ur yt chanel name
+global.socialm = "GitHub: SoArLozzy_xD" //ur github or insta name
+global.location = "India, Kolkata, West Bengal" //ur location
 
 //bot bomdy 
 global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
+global.ownertag = '6289372021' //ur tag number
 global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
+global.linkz = "https://www.youtube.com/channel/UCpyWZlckiJI30n8Q9SiPVdA" //your theme url which will be displayed on whatsapp
+global.websitex = "Coming Soon" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.packname = "Corpse Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
+global.author = "🦄Dream Guy Lucifer_" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
