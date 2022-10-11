cbe5d86ab8a5f20532072316f2a74c9cf4d8d825
ahmadkings <98577932+ahmadkings@users.noreply.github.com>
2022-10-12 00:01:43 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['923076967435'] //923076967435
+global.ownername = "🦄Devil WhatsApp Bot" //Ahmad Malik
+global.ytname = "YT: Malikfilemaker" //https://youtube.com/channel/UCFP8CpLcTY8MilJB4L73gWA
+global.socialm = "GitHub: ahmadkings" //ahmadkings
+global.location = "India, Mizoram, Aizawl" //Pakistan
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
+global.owner = ['923076967435']
+global.ownertag = '923076967435' //923076967435
+global.botname = 'Devil WhatsApp Bot' //King WhatsApp Bot
+global.linkz = "https://youtube.com/channel/UCFP8CpLcTY8MilJB4L73gWA" //https://youtube.com/channel/UCFP8CpLcTY8MilJB4L73gWA
+global.websitex = "https://youtube.com/channel/UCFP8CpLcTY8MilJB4L73gWA" //https://youtube.com/channel/UCFP8CpLcTY8MilJB4L73gWA
+global.botscript = 'https://github.com/ahmadkings/CheemsBot-MD4' //script link
+global.reactmoji = "👿" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.packname = "Devil Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
+global.author = "🦄Ahmad Malik" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['923076967435'] //923327264768
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
