6c1d51d8d63391e33e1ae6fd0d5d344ad47a5c56
DarLord456 <113214653+DarLord456@users.noreply.github.com>
2022-10-16 16:13:35 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,16 +54,16 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['+2120644029110'] //ur owner number
+global.ownername = " A K A I N U " //ur owner name
+global.ytname = "YT: Shaggy-Senpai" //ur yt chanel name
+global.socialm = "GitHub: DarLord456" //ur github or insta name
+global.location = "Morocco, Casablanca, Alfida" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
+global.owner = ['+2120641235622']
+global.ownertag = '+2120641235622' //ur tag number
+global.botname = 'AKAINU' //ur bot name
 global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
