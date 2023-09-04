fe5d168fb746cbca48089bc429550827ea805df0
Fisachy <143998184+Fisachy@users.noreply.github.com>
2023-09-04 13:39:31 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,29 +54,29 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['94715328230'] //ur owner number
+global.ownername = "Dinusha" //ur owner name
+global.ytname = "YT: dinusha" //ur yt chanel name
+global.socialm = "Insta: dinusha_shehan1" //ur github or insta name
+global.location = "srilanka, colombo, colombo" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
+global.owner = ['94715328230']
+global.ownertag = '94715328230' //ur tag number
+global.botname = 'Dinusha' //ur bot name
 global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
 global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.author = "🦄Dream Guy dinusha" //ur sticker watermark author
 
 //Bot theme media
-global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
-global.log0 = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur logo pic
-global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
-global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+global.thum = fs.readFileSync(".http://telegra.ph/file/cd39c2b6d73ce4a8720ec.jpg") //ur thumb pic
+global.log0 = fs.readFileSync(".http://telegra.ph/file/cd39c2b6d73ce4a8720ec.jpg") //ur logo pic
+global.err4r = fs.readFileSync(".http://telegra.ph/file/cd39c2b6d73ce4a8720ec.jpg") //ur error pic
+global.thumb = fs.readFileSync(".http://telegra.ph/file/cd39c2b6d73ce4a8720ec.jpg") //ur thumb pic
 
 //damtabase
 global.premium = ['916909137213'] //ur premium numbers
