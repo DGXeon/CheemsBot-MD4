72ea76aae2030ef0580127076850016512c18023
ikram khan <116757493+Ikramkhaan@users.noreply.github.com>
2023-12-08 19:43:25 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
+global.vcardowner = ['923058933961'] //ur owner number
+global.ownername = "🦄 𝕀𝕜𝕣𝕒𝕞 𝕂𝕙𝕒𝕟" //ur owner name
+global.ytname = "YT: https://youtube.com/@HamaraTrend?si=NfsN1VNpKD4dfwlQ" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.location = "Pakistan, KPK, Peshawar" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
+global.owner = ['923058933961']
+global.ownertag = '923058933961' //ur tag number
+global.botname = 'HEART-HACKER' //ur bot name
+global.linkz = "https://youtube.com/@HamaraTrend?si=NfsN1VNpKD4dfwlQ" //your theme url which will be displayed on whatsapp
+global.websitex = "https://youtube.com/@HamaraTrend?si=NfsN1VNpKD4dfwlQ" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.packname = "𝕀𝕜𝕣𝕒𝕞 𝕂𝕙𝕒𝕟 Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
+global.author = "🦄Dream Guy 𝕀𝕜𝕣𝕒𝕞 𝕂𝕙𝕒𝕟" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['923058933961'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
